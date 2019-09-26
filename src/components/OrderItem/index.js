1b790c1dd9929f0820c673f4edfcd4d0e020c997
yanyan.sun <yanyan.sun@weimob.com>
2019-09-26 12:03:43 +0800

    update

--- src/components/OrderItem/index.js
+++ src/components/OrderItem/index.js
@@ -2,26 +2,109 @@ import React, { Component } from 'react'
 import './style.css'
 
 class OrderItem extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      editing: false,
+      stars: props.data.stars || 0,
+      comment: props.data.comment || ''
+    }
+  }
+
   render() {
-    const { shop, product, price, picture, ifCommented } = this.props.item
+    const { shop, product, price, picture, ifCommented } = this.props.data
     return (
       <div className="orderItem">
-        <div className="orderItem__picContainer">
-          <img className="orderItem__pic" src={picture} alt=""></img>
-        </div>
-        <div className="orderItem__content">
-          <div className="orderItem__product">{product}</div>
-          <div className="orderItem__shop">{shop}</div>
-          <div className="orderItem__detail">
-            <div className="orderItem__price">{price}</div>
-            {/* <div><button className={["orderItem__btn", ifCommented ? 'orderItem__btn--gray' : 'orderItem__btn--red'].join(' ')}>评价</button></div> */}
-            {/* es6语法写动态class 推荐 */}
-            <div><button className={`orderItem__btn ${ifCommented ? 'orderItem__btn--gray' : 'orderItem__btn--red'}`}>{ifCommented ? '已评价' : '评价'}</button></div>
+        <div className="orderItem__container">
+          <div className="orderItem__picContainer">
+            <img className="orderItem__pic" src={picture} alt=""></img>
+          </div>
+          <div className="orderItem__content">
+            <div className="orderItem__product">{product}</div>
+            <div className="orderItem__shop">{shop}</div>
+            <div className="orderItem__detail">
+              <div className="orderItem__price">{price}</div>
+              {/* <div><button className={["orderItem__btn", ifCommented ? 'orderItem__btn--gray' : 'orderItem__btn--red'].join(' ')}>评价</button></div> */}
+              {/* es6语法写动态class 推荐 */}
+              <div>
+                {/* <button 
+                  className={`orderItem__btn ${ifCommented ? 'orderItem__btn--gray' : 'orderItem__btn--red'}`}
+                  onClick={this.clickComment}
+                >{ifCommented ? '已评价' : '评价'}</button> */}
+                {
+                  ifCommented ? (<button className="orderItem__btn orderItem__btn--gray">已评价</button>) :
+                  (<button className="orderItem__btn orderItem__btn--red" onClick={this.handleOpenEditArea}>评价</button>)
+                }
+              </div>
+            </div>
           </div>
         </div>
+        {this.state.editing && this.renderEditArea()}
       </div>
     );
   }
+
+  renderEditArea() {
+    return (
+      <div className="orderItem__commentContainer">
+        <textarea onChange={this.handleCommentChange} value={this.state.comment} className="orderItem__comment" />
+        {this.renderStars()}
+        <button className="orderItem__btn orderItem__btn--red" onClick={this.handleSubmitComment}>提交</button>
+        <button className="orderItem__btn orderItem__btn--gray" onClick={this.handleCancleComment}>取消</button>
+      </div>
+    )
+  }
+
+  renderStars() {
+    const { stars } = this.state
+    return (
+      <div className="orderItem__startsContainer">
+        { [1,2,3,4,5].map((e, i) =>  {
+          const lightClass = e <= stars ? 'orderItem__star--light' : ''
+          return (
+            <span key={i} className={"orderItem__star " + lightClass} onClick={this.handleClickStars.bind(this, e)}>★</span> 
+          )
+        }) }
+      </div>
+    )
+  }
+
+  handleOpenEditArea = () => {
+    this.setState({
+      editing: true
+    })
+  }
+
+  handleCommentChange = (e) => {
+    this.setState({
+      comment: e.target.value
+    })
+  }
+
+  handleClickStars = (stars) => {
+    this.setState({
+      stars
+    })
+  }
+
+  handleCancleComment = () => {
+    this.setState({
+      editing: false,
+      comment: this.props.data.comment || '',
+      stars: this.props.data.stars || 0
+    })
+  }
+
+  handleSubmitComment = () => {
+    const { id } = this.props.data
+    const { comment, stars } = this.state
+    this.props.onSubmit(id, comment, stars)
+    this.setState({
+      editing: false,
+    })
+  } 
+
 }
 
 export default OrderItem;
--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -4,33 +4,43 @@ import "./style.css"
 import OrderItem from '../OrderItem'
 
 class OrderList extends Component {
-  render() {
-    const list = [
-      {
-        shop: '院落创意菜',
-        picture: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569342464796&di=f2393d837ea3088ff046363a1da7973a&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01de245840e23ea8012060c8d78b48.JPG%401280w_1l_2o_100sh.jpg',
-        product: '百香果 1 扎',
-        price: 29.9,
-        ifCommented: false
-      }, 
-      {
-        shop: '人在茶在',
-        picture: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569342494484&di=3a8b8e51dce1c06286784bda1b8414f0&imgtype=0&src=http%3A%2F%2Fpic176.nipic.com%2Ffile%2F20180807%2F7235424_131719065000_2.jpg',
-        product: '金桔柠檬茶',
-        price: 20,
-        ifCommented: true
-      },
-      {
-        shop: '一点点',
-        picture: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569342652776&di=03a468d4ea50032fb5da49c5093d75c8&imgtype=0&src=http%3A%2F%2Fimg.ixinwei.com%2Fiww201802%2F111595.jpg',
-        product: '珍珠奶茶',
-        price: 14,
-        ifCommented: true
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/mock/order.json').then(res => {
+      if(res.ok) {
+        res.json().then(data => {
+          this.setState({
+            list: data.dataList
+          })
+        })
       }
-    ]
+    })
+  }
+
+  handleSubmit = (id, comment, stars) => {
+    // fetch('saveComment').then(() => {})
+    const newList = this.state.list.map((e, i) => {
+      return e.id === id ? 
+      {
+        ...e, stars, comment, ifCommented: true
+      } : e
+    })
+    this.setState({
+      list: newList
+    })
+  }
+
+  render() {
+    const { list = [] } = this.state
     return (
       <div className="orderList">
-        {list.map((e,i) => <OrderItem item={e} key={i}/>)}
+        {list.length && list.map((e,i) => <OrderItem data={e} key={i} onSubmit={this.handleSubmit}/>)}
       </div>
     );
   }
